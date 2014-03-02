5310b23a4adf5312a73936f1a831f6fd1158e935
mdabski <dabrowski.mark@gmail.com>
2014-03-02 21:23:45 +0000

    more tests added to beer model

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,2 +1,7 @@
 class Beer < ActiveRecord::Base
+  
+  validates :brewer, presence: true
+  validates :name, presence: true
+  validates :style, presence: true
+  
 end
--- spec/models/beer_spec.rb
+++ spec/models/beer_spec.rb
@@ -1,11 +1,36 @@
 require 'spec_helper'
 
 describe Beer do
-  it "is valid with a brewer, name and style" do
+  it "is valid with a brewer, name, style and url" do
     beer = Beer.new(
       brewer: 'Broadhead',
       name: 'Dark Horse',
-      style: 'Stout')
+      style: 'Stout',
+      url: 'www.broadhead.com')
+    expect(beer).to be_valid
+  end
+  
+  it "is invalid without a brewer" do
+    beer = Beer.new(brewer: nil)
+    expect(beer).to have(1).errors_on(:brewer)
+  end
+  
+  it "is invalid without a name" do
+    beer = Beer.new(name: nil)
+    expect(beer).to have(1).errors_on(:name)
+  end
+  
+  it "is invalid without a style" do
+    beer = Beer.new(style: nil)
+    expect(beer).to have(1).errors_on(:style)
+  end
+  
+  it "is valid without a url" do
+    beer = Beer.new(
+      brewer: 'Broadhead',
+      name: 'Dark Horse',
+      style: 'Stout',
+      url: nil)
     expect(beer).to be_valid
   end
   
