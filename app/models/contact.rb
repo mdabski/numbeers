7a72b6713851a6e82c2b3498fc8a075d9676b7f3
mdabski <dabrowski.mark@gmail.com>
2014-12-10 01:31:57 +0000

    started to add ability to manage contact information

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,6 +2,9 @@ class Contact < ActiveRecord::Base
   belongs_to :user
   has_many :transactions
   
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true
   
   def init()
     assign_numbeer_id() 
