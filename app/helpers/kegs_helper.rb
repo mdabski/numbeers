137d976b6740a829a7bcfccc9fe9be36661973da
Mark Dabrowski <dabrowski.mark@gmail.com>
2015-08-28 12:19:28 +0000

    Keg Freshness

--- app/helpers/kegs_helper.rb
+++ app/helpers/kegs_helper.rb
@@ -1,2 +1,11 @@
 module KegsHelper
+	def get_freshness()
+    latest_keg = Keg.last
+    if latest_keg.nil?
+      "No Keg!?"
+    else
+    time_ago_in_words(latest_keg.start_date) + " ago"
+      #lastpour.created_at.strftime("%a - %b %d, %Y \n %I:%M %p")
+    end
+  end
 end
